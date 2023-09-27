931fa719f1e576f64383790f00d2fac3f0a9e774
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-09-27 11:55:10 +0700

    new django project

--- Novelty/asgi.py
+++ Novelty/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for NoveltyChecker project.
+ASGI config for Novelty project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NoveltyChecker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Novelty.settings')
 
 application = get_asgi_application()
--- Novelty/wsgi.py
+++ Novelty/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NoveltyChecker project.
+WSGI config for Novelty project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NoveltyChecker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Novelty.settings')
 
 application = get_wsgi_application()
