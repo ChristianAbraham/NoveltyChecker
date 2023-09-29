2152505cb497f529419a2f7cb5161ca5b813c8c9
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-09-29 11:16:28 +0700

    views added

--- NoveltyChecker/views.py
+++ NoveltyChecker/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "NoveltyChecker/index.html")
+
+def login(request):
+    return render(request, "NoveltyChecker/login.html")
+
+def register(request):
+    return render(request, "NoveltyChecker/register.html")
+
+def account(request):
+    return render(request, "NoveltyChecker/account.html")
+
+def report(request):
+    return render(request, "NoveltyChecker/report.html")
+
+def searchResult(request):
+    return render(request, "NoveltyChecker/listrecommendation.html")
+
+def search(request):
+    return render(request, "NoveltyChecker/search.html")
+
